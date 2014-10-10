52942fc20b07e11769601aea1b3ffe58b5650da3
Zach Ohara <zjohara@gmail.com>
2014-10-10 02:15:24 -0700

    SimpleMovement.c
    Created simplemovement for ONLY wrappers of movement.c like
    moveforward() or trafe().
    
    Lots to be done here but I'm wayy too tired to even be doing this.
    
    Deleted more journal because it sucks and who needs it. Just look on the
    commit history.

--- movement.c
+++ movement.c
@@ -64,16 +64,4 @@ static byte scaleTo(byte value, ubyte range[3], ubyte scale[3]) {
 	return (byte)range[0] + (posInR * (float)range[2]);
 }
 
-//All methods should be moved to simplemovement
-
-//NOT IMPLEMENTED YET:
-void moveFoward(byte power, short distance/*We need to decide: cm or in?*/) {}
-void moveStrafe(byte power, short distance/*We need to decide: cm or in?*/) {}
-void turnInPlace(byte power, short deg) {}
-void turnSwing(byte power, short deg) {}
-
-//Low priority
-//move in a spiral, like r = x in a polar graph
-//variables may need changing
-void turnSpiral(byte power, short distance/*We need to decide: cm or in?*/, short deg) {}
-void moveDiagonal(char vect/*which diagonal?*/, byte power, short distance/*We need to decide: cm or in?*/) {}
+//All methods have been moved to simplemovement
---
+++ simplemovement.h
@@ -0,0 +1,17 @@
+//simplemovement.h
+//Contains wrappers for making movement.c functions easier.
+
+/*
+//NOT IMPLEMENTED YET:
+void moveFoward(byte power, short distance/*We need to decide: cm or in?*/) {}
+void moveStrafe(byte power, short distance/*We need to decide: cm or in?*/) {}
+void turnInPlace(byte power, short deg) {}
+void turnSwing(byte power, short deg) {}
+
+//Low priority
+//move in a spiral, like r = x in a polar graph
+//variables may need changing
+void turnSpiral(byte power, short distance/*We need to decide: cm or in?*/, short deg) {}
+void moveDiagonal(char vect/*which diagonal?*/, byte power, short distance/*We need to decide: cm or in?* {}
+
+*/
