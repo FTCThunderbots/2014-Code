3872d987c7c3ae96c34c2e29666c8108e1d5f396
Zach Ohara <zjohara@gmail.com>
2014-10-10 16:27:59 -0700

    Organized method header
    Clearly shows which methods we need to write. I can probably tackle most
    of it.

--- simplemovement.h
+++ simplemovement.h
@@ -1,17 +1,22 @@
 //simplemovement.h
 //Contains wrappers for making movement.c functions easier.
 
-/*
-//NOT IMPLEMENTED YET:
-void moveFoward(byte power, short distance/*We need to decide: cm or in?*/) {}
-void moveStrafe(byte power, short distance/*We need to decide: cm or in?*/) {}
-void turnInPlace(byte power, short deg) {}
-void turnSwing(byte power, short deg) {}
+//Need to be implemented
+void drive(short power);
+void driveIn(short dist, short power);
+void driveCm(short dist, short power);
+void strafe(short power);
+void strafeIn(short dist, short power);
+void strafeCm(short dist, short power);
+void rotate(short power);
+void rotateDeg(short degs, short power);
+void rotateRad(short rads, short power);
 
-//Low priority
-//move in a spiral, like r = x in a polar graph
-//variables may need changing
-void turnSpiral(byte power, short distance/*We need to decide: cm or in?*/, short deg) {}
+//Other ideas:
+void swingTurn(short power); //and with degrees/rads
+
+//Low priority: (Daniel's ideas)
+void turnSpiral(byte power, short distance, short deg) {}
 void moveDiagonal(char vect/*which diagonal?*/, byte power, short distance/*We need to decide: cm or in?* {}
 
 */
