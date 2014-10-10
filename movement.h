33afe57bd649d7fab1ef7f79b42f894e5f946a7a
Daniel <danielgrimshaw42@gmail.com>
2014-10-09 18:59:21 -0700

    Update movement.h
    Added most, if not all, of the method headers that we will need;

--- movement.h
+++ movement.h
@@ -24,3 +24,15 @@ byte fmax(byte a, byte b);
 byte absMax(byte a, byte b);
 static byte scaleTo(byte value, ubyte range[3], ubyte scale[3]);
 void setMovement(byte forward, byte right, byte clockwise);
+
+//NOT IMPLEMENTED YET:
+void moveFoward(byte power, short distance/*We need to decide: cm or in?*/);
+void moveStrafe(byte power, short distance/*We need to decide: cm or in?*/);
+void turnInPlace(byte power, short deg);
+void turnSwing(byte power, short deg);
+
+//Low priority
+//move in a spiral, like r = x in a polar graph
+//variables may need changing
+void turnSpiral(byte power, short distance/*We need to decide: cm or in?*/, short deg); 
+void moveDiagonal(char vect/*which diagonal?*/, byte power, short distance/*We need to decide: cm or in?*/);
