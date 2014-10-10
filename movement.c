d9b0799f4813f9ecde9d46161f8d1abfeedae893
Daniel <danielgrimshaw42@gmail.com>
2014-10-09 19:00:52 -0700

    Update movement.c
    now matches movement.h

--- movement.c
+++ movement.c
@@ -66,3 +66,15 @@ static byte scaleTo(byte value, ubyte range[3], ubyte scale[3]) {
 	float posInR = value / (range[2]);
 	return (byte)range[0] + (posInR * (float)range[2]);
 }
+
+//NOT IMPLEMENTED YET:
+void moveFoward(byte power, short distance/*We need to decide: cm or in?*/) {}
+void moveStrafe(byte power, short distance/*We need to decide: cm or in?*/) {}
+void turnInPlace(byte power, short deg) {}
+void turnSwing(byte power, short deg) {}
+
+//Low priority
+//move in a spiral, like r = x in a polar graph
+//variables may need changing
+void turnSpiral(byte power, short distance/*We need to decide: cm or in?*/, short deg) {}
+void moveDiagonal(char vect/*which diagonal?*/, byte power, short distance/*We need to decide: cm or in?*/) {}
