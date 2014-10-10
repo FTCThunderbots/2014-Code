def9669d7ce4cdc1bb71066b6e7d7e99f125fb02
Daniel <danielgrimshaw42@gmail.com>
2014-10-09 18:35:48 -0700

    Updated movement.h
    Method declarations for new functions in movement.c;
    Updated old method declarations;
    Added comment box for notes (should be deprecated after code is working to perfection)

--- movement.h
+++ movement.h
@@ -1,5 +1,26 @@
-/*movement.h
- *include this header to access these two methods
+/* movement.h:
+ * Include this header to access these methods for movement
  */
-static short scaleTo(short value, short range[3], short scale[3]);
-void setMovement(short forward, short right, short clockwise);
+ 
+ ///////////////////////////////////////////////////////////////
+ /*                       Comment Box:                        */
+ /* -Keep all methods except scaleTo non-static so that they  */
+ /*     can be accessed from all codes that include this file */
+ /*                                                           */
+ /*                                                           */
+ /*                                                           */
+ /*                                                           */
+ /*                                                           */
+ /*                                                           */
+ /* -Perhaps we should typedef a movement variables struct?   */
+ /*      We could then use that as our parameters to scaleTo  */
+ /*     and setMovement                                       */
+ /*                                                           */
+ /*                                                           */
+ /*                                                           */
+ ///////////////////////////////////////////////////////////////
+
+byte fmax(byte a, byte b);
+byte absMax(byte a, byte b);
+static byte scaleTo(byte value, ubyte range[3], ubyte scale[3]);
+void setMovement(byte forward, byte right, byte clockwise);
