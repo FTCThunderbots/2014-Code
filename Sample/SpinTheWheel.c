0084c4f136f8fb0768ed4f6517ef1b3411736739
Zach Ohara <zjohara@gmail.com>
2014-10-11 01:10:44 -0700

    Finished the example program (spin the wheel)
    SpinTheWheel is a sample program that takes input from joy1_y1 and sets
    the speed of a motor on S1_C1_1 accordingly

--- Sample/SpinTheWheel.c
+++ Sample/SpinTheWheel.c
@@ -10,13 +10,19 @@ void setWheelPower(int cheeseburger){
 	int i = 2;
 }
 
-int
+int correctJoystick(int joystick) {
+	if (abs(joystick) <= 15) // if joystick is between -15 and +15
+		return 0; //completely disregard the input from the joystick
+	else
+		return joystick //leave the input unchanged
 
 task main()
 {
 	while (true) {
 		 getJoystickSettings(joystick);
-		 int wheelPower = joystick.joy1_y1 / 1.28;
+		 //int wheelPower = joystick.joy1_y1 / 1.28; 				<-- Old way
+		 int wheelPower = correctJoystick(joystick.joy1_y1); // 	<-- New way
+		 wheelPower /= 1.28 //divide wheelPower by 1.28
 		 setWheelPower(wheelPower)
 		 wait1Msec(10);
 	}
