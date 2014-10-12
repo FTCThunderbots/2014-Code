47e1860d3edd058254f28ac14be3d0720af568a6
Zach Ohara <zjohara@gmail.com>
2014-10-12 12:02:06 -0700

    Updating types- Part 2
    The data types used in simplemovement.c were atrocious.

--- api/simplemovement.c
+++ api/simplemovement.c
@@ -7,31 +7,31 @@
 
 //look in header for functions that need implementations
 
-void drive(int power) {
+void drive(byte power) {
 	setMovement(power, 0, 0);
 }
 
-void strafe(int power) {
+void strafe(byte power) {
 	setMovement(0, power, 0);
 }
 
-void rotate(int power) {
+void rotate(byte power) {
 	setMovement(0, 0, power);
 }
 
-void swingTurn(int direction, int power) {
+void swingTurn(byte direction, byte power) {
 	// direction: 0 for left, 1 for right;
 	direction = direction == 0 ? -1 : 1;
 	setMovement(power, 0, direction * power)
 }
 
-void moveDiagonal(int vector, int power) {
+void moveDiagonal(byte vector, byte power) {
 	// vector is 0 to 3, with 0 being straight forward, and 3 being back/left
 	// use negative power to move in any of the other four directions
 	
 	//consider using pothagorean theorem to make sure overall speed is equal in all directions with a constant power
-	int drive = 1; 
-	int strafe = 1;
+	byte drive = 1; 
+	byte strafe = 1;
 	if (power < 0)
 		strafe = -1;
 	if (vector == 0)
--- api/simplemovement.h
+++ api/simplemovement.h
@@ -3,34 +3,34 @@
 
 #include "api.h"
 
-void drive(int power);
-void strafe(int power);
+void drive(byte power);
+void strafe(byte power);
 //rotate function should be depracated, nearly uncontrolable
-void rotate(int power);
-void swingTurn(int direction, int power);
-void moveDiagonal(int vector, int power);
+void rotate(byte power);
+void swingTurn(byte direction, byte power);
+void moveDiagonal(byte vector, byte power);
 
 //Need to be implemented
-void driveTicks(int ticks, int power);
-void driveIn(int dist, int power);
-void driveCm(int dist, int power);
+void driveTicks(byte ticks, byte power);
+void driveIn(byte dist, byte power);
+void driveCm(byte dist, byte power);
 
-void strafeTicks(int ticks, int power);
-void strafeIn(int dist, int power);
-void strafeCm(int dist, int power);
+void strafeTicks(byte ticks, byte power);
+void strafeIn(byte dist, byte power);
+void strafeCm(byte dist, byte power);
 
-void rotateTicks(int ticks, int power);
-void rotateDeg(int degs, int power);
-void rotateRad(int rads, int power);
+void rotateTicks(byte ticks, byte power);
+void rotateDeg(byte degs, byte power);
+void rotateRad(byte rads, byte power);
 
 //Other ideas:
-void swingTurnTicks(int ticks, int direction, int power);
-void swingTurnDeg(int degs, int directions, int power);
-void swingTurnRad(int rads, int directions, int power);
+void swingTurnTicks(byte ticks, byte direction, byte power);
+void swingTurnDeg(byte degs, byte directions, byte power);
+void swingTurnRad(byte rads, byte directions, byte power);
 
-void moveDiagonalTicks(int dist, int vector, int power);
-void moveDiagonalIn(int dist, int vector, int power);
-void moveDiagonalCm(int dist, int vector, int power);
+void moveDiagonalTicks(byte dist, byte vector, byte power);
+void moveDiagonalIn(byte dist, byte vector, byte power);
+void moveDiagonalCm(byte dist, byte vector, byte power);
 
 //Low priority: (Daniel's ideas)
-void turnSpiral(byte power, int distance, int deg);
+void turnSpiral(byte power, byte distance, byte deg);
