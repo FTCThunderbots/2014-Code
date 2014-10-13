f541916dcafe9f2fc55a7c1dc9fa152f03574182
Daniel <danielgrimshaw42@gmail.com>
2014-10-13 10:02:54 -0700

    Update settings.c
    the register keyword allows for fast memory access by storing variables in the cpu register;
    MAY NOT BE SUPPPORTED BY ROBOTC!

--- api/settings.c
+++ api/settings.c
@@ -5,21 +5,21 @@
 #include "api.h"
 
 // Joysticks
-const int JOYSTICK_MIN_VALUE = 10; //min value where the reading is accepted as an input
-const int JOYSTICK_MAX_VALUE = 128; //should only be changed if a different joystick is used
+register const int JOYSTICK_MIN_VALUE = 10; //min value where the reading is accepted as an input
+register const int JOYSTICK_MAX_VALUE = 128; //should only be changed if a different joystick is used
 
 // Movement
-const int DRIVE_MIN_POWER = 0; //power cannot be in the open interval (0, min)
-const int DRIVE_MAX_POWER = 100; //power cannot exceed max
-const int STRAFE_MIN_POWER = 0;
-const int STRAFE_MAX_POWER = 100;
-const int TURN_MIN_POWER = 0;
-const int TURN_MAX_POWER = 100;
-const float MOVE_POWER_SCALE = 1.0; //used for imposing speed limits
+register const byte DRIVE_MIN_POWER = 0; //power cannot be in the open interval (0, min)
+register const byte DRIVE_MAX_POWER = 100; //power cannot exceed max
+register const byte STRAFE_MIN_POWER = 0;
+register const byte STRAFE_MAX_POWER = 100;
+register const byte TURN_MIN_POWER = 0;
+register const byte TURN_MAX_POWER = 100;
+register const float MOVE_POWER_SCALE = 1.0; //used for imposing speed limits
 
 // Motor Constants
-const int MOTOR_MIN_POWER = 0; //not yet implemented
-const int MOTOR_MAX_POWER = 100;
+register const byte MOTOR_MIN_POWER = 0; //not yet implemented
+register const byte MOTOR_MAX_POWER = 100;
 
 //Todo: wheel diameter and tick counts
 //
