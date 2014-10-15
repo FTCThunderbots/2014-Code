f857b2340fbb84c45cd6ed222dde187ac1f1b5d1
Zach Ohara <zjohara@gmail.com>
2014-10-14 23:39:59 -0700

    Tried some things. Doesn't look like they work.
    I was trying to get creative with timer.c to avoid having to use a
    linked list, but at this point, it seems inevitable. I'll have Daniel
    look at this and decide if it's worth saving.

--- api/timer.c
+++ api/timer.c
@@ -3,58 +3,54 @@
 
 #include "timer.h"
 
+typedef char byte;
+typedef int bool;
+#define true 1
+#define false 0
+
 static long currentTime = 0;
-static TimerLinkedList_t timerSet;
-timerSet.length = 0;
+static Timer_t timerSetM[0];
+static byte timers = 0;
+static Timer_t *timerSet = timerSetM;
 
 //in user file:
-//Timer *usetimername = newTimer()
+//Timer_t **usetimername = newTimer();
+//startTimer(usertimername);
 
-/*typedef struct TimerListNode {
-   TimerListNode_t *next;
-   
-   float seconds;
-	long miliseconds;
-	bool running;
-	long start;
-	long previousTime;
-} TimerListNode_t;
-
-typedef struct TimerLinkedList {
-   TimerListNode_t head;
-   byte length;
-} TimerLinkedList_t;*/
-
-Timer_t * newTimer(void) {
-	Timer_t newMasterTimerSet[timerSet.length + 1];
-	for (byte i = 0; i < newTimerSet.length; i++)
-		newTimerSet[i] = timerSet[i];
+Timer_t **newTimer() {
+	Timer_t newMaster[timers + 1];
+	int i;
+	for (i = 0; i < timers+1; i++)
+		newMaster[i] = timerSetM[i];
 	Timer_t newTimer;
-	newTimer.seconds = 0;
-	newTimer.miliseconds = 0;
-	newTimer.running = false;
-	newTimer.previousTime = 0;
-	newTimerSet[newTimerSet.length - 1] = newTimer;
-	return &newTimerSet[newTimerSet.length - 1];
+	newTimer.seconds = 0; //          }
+	newTimer.miliseconds = 0;//       } Code specific to timer type
+	newTimer.running = false;//       }
+	newTimer.previousTime = 0;//      }
+	newMaster[timers] = newTimer;
+	timers++;
+	timerSetM[timers] = newTimer;
+	timerSet = timerSetM;
+	return (timerSet + (timers-1));
 }
 
-void startTimer(Timer_t timer) {
-	timer.previousTime = timer.miliseconds;
-	timer.running = true;
-	timer.start = currentTime;
+void startTimer(Timer_t **timer) {
+	(**timer).previousTime = (**timer).miliseconds;
+	(**timer).running = true;
+	(**timer).start = currentTime;
 }
 
-void clearTimer(Timer_t timer) {
-	timer.previousTime = 0;
-	timer.seconds = 0;
-	timer.miliseconds = 0;
+void clearTimer(Timer_t **timer) {
+	(**timer).previousTime = 0;
+	(**timer).seconds = 0;
+	(**timer).miliseconds = 0;
 }
 
 void stopTimer(Timer_t timer) {
 	timer.running = false;
-	
+}	
 
-task timing() {
+//task timing() {
 	//update timer stuff in here:
 	
 	/* General structure:
@@ -62,7 +58,7 @@ task timing() {
 	 * In each iteration, for all elements in timerSet[]:
 	 * 		If running, update seconds and miliseconds fields
 	 */
-}
+//}
 
 float runtime() {
 	return (float)currentTime / 1000;
--- api/timer.h
+++ api/timer.h
@@ -1,12 +1,27 @@
 // timer.h
 // Header file for timer.c
 
-#include "api.h"
+//#include "api.h"
 
-Timer_t * newTimer();
+typedef char byte;
+typedef int bool;
+#define true 1
+#define false 0
+
+typedef struct Timer {
+   float seconds;
+	long miliseconds;
+	bool running;
+	long start;
+	long previousTime;
+} Timer_t;
+
+/*
+Timer_t **newTimer();
 void startTimer(Timer_t timer);
 void stopTimer(Timer_t timer);
 void clearTimer(Timer_t timer);
-task timing();
+//task timing();
 
 float runtime();
+*/
