86b9fe0ca01d2983a4bcb249c7f6496c973aac5d
Alec <Alecheeky@gmail.com>
2014-10-17 19:25:56 -0700

    Fixed auto and spinthewheel
    ;)

--- Sample/SpinTheWheel.c
+++ Sample/SpinTheWheel.c
@@ -7,14 +7,14 @@
 
 void setWheelPower(int cheeseburger){
 	motor[wheel] = cheeseburger;
-	int i = 2;
+	//int i = 2;
 }
 
 int correctJoystick(int joystick) {
 	if (abs(joystick) <= 15) // if joystick is between -15 and +15
 		return 0; //completely disregard the input from the joystick
 	else
-		return joystick //leave the input unchanged
+		return joystick; //leave the input unchanged
 }
 
 task main()
@@ -23,8 +23,8 @@ task main()
 		 getJoystickSettings(joystick);
 		 //int wheelPower = joystick.joy1_y1 / 1.28; 				<-- Old way
 		 int wheelPower = correctJoystick(joystick.joy1_y1); // 	<-- New way
-		 wheelPower /= 1.28 //divide wheelPower by 1.28
-		 setWheelPower(wheelPower)
+		 wheelPower /= 1.28; //divide wheelPower by 1.28
+		 setWheelPower(wheelPower);
 		 //wait1Msec(10);
 	}
 }
