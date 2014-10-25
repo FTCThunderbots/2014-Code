dc857ac469a54f0da788ba13a3f83e002957fdc0
Zachary Waldron <wewingames222@gmail.com>
2014-10-25 12:56:02 -0700

    Work In Progress Coordinate System
    W.I.P. Many functions are to be added, and many more variables are to be
    defined in order to be stable.

---
+++ wait-to-add/WIPCoordSystem.c
@@ -0,0 +1,23 @@
+// #include statements
+#include "math.c"
+#include "JoystickDriver.c"
+#include "api.c"
+#include "unit_converter.c"
+//#pragma
+
+float x = 0.0, z = 0.0;
+
+typedef enum {INCHES, FEET, CENTIMETER, METER} length_t;
+
+static void UpdateCoords(float degrees, length_t CIRCUM_TYPE) {
+		/*
+			1. Divide ticks by 1440 then multiply by circumference (Gives you how far it moved in the units
+			you have used for circumference.
+			2. Convert the units to feet.
+		*/
+		byte ticks;
+		float changeCoord = (ticks / 1440) * convertUnits(CIRCUM_TYPE, FEET, degrees);
+}
+
+#pragma //put the nameOfMotor here
+nMotorEncoder[nameOfMotor];
