f24daaf414748d328ea73296c5afb26e2b34f293
Zach Ohara <zjohara@gmail.com>
2014-10-25 16:09:01 -0700

    Updated linesWritten
    Updated the python file. The current line count is exactly 1765 between
    all current files.

--- linesWritten.py
+++ linesWritten.py
@@ -4,6 +4,8 @@
 # Please keep this list alphebetized
 filesToCheck = ('api/api.c',
 	'api/background.c',
+	'api/encoders.c',
+	'api/encoders.h',
 	'api/functions.c',
 	'api/functions.h',
 	'api/math.c',
@@ -19,12 +21,27 @@ filesToCheck = ('api/api.c',
 	'api/simplemovement.h',
 	'api/timer.c',
 	'api/timer.h',
-	'autonomous/drive off ramp auto.c',
-	'config/default config.c',
+	
+	'autonomous/drive_off_ramp_auto.c',
+	
+	'config/2013_robot_config.c',
+	'config/default_config.c',
+	
+	'sample/first_driving_program.c',
 	'sample/mecanumDrive.h',
 	'sample/SensorTesting.c',
 	'sample/SpinTheWheel.c',
+	
 	'tele-op/main teleop.c',
+	
+	'testing/main_test_program.c',
+	'testing/setMovementTest.c',
+	'testing/turn_test.c',
+	
+	'wait-to-add/unit_converter.c',
+	'wait-to-add/unitConverter.c',
+	'wait-to-add/WIPCoordSystem.c',
+	
 	'linesWritten.py')
 
 def lengthOf(filename):
