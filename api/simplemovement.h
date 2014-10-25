9880e70b47cb8af71375c7e128e9ab7ed106d2ee
pranavmathur <pranavmathur99@gmail.com>
2014-10-24 17:06:15 -0700

    Commented out more unimplemented header
    in simplemovement.h

--- api/simplemovement.h
+++ api/simplemovement.h
@@ -11,26 +11,26 @@ void swingTurn(byte direction, byte power);
 void moveDiagonal(byte vector, byte power);
 
 //Need to be implemented
-void driveTicks(byte ticks, byte power);
-void driveIn(byte dist, byte power);
-void driveCm(byte dist, byte power);
+//void driveTicks(byte ticks, byte power);
+//void driveIn(byte dist, byte power);
+//void driveCm(byte dist, byte power);
 
-void strafeTicks(byte ticks, byte power);
-void strafeIn(byte dist, byte power);
-void strafeCm(byte dist, byte power);
+//void strafeTicks(byte ticks, byte power);
+//void strafeIn(byte dist, byte power);
+//void strafeCm(byte dist, byte power);
 
-void rotateTicks(byte ticks, byte power);
+//void rotateTicks(byte ticks, byte power);
 void rotateDeg(int degs, byte power);
-void rotateRad(byte rads, byte power);
+//void rotateRad(byte rads, byte power);
 
 //Other ideas:
-void swingTurnTicks(byte ticks, byte direction, byte power);
-void swingTurnDeg(byte degs, byte directions, byte power);
-void swingTurnRad(byte rads, byte directions, byte power);
+//void swingTurnTicks(byte ticks, byte direction, byte power);
+//void swingTurnDeg(byte degs, byte directions, byte power);
+//void swingTurnRad(byte rads, byte directions, byte power);
 
-void moveDiagonalTicks(byte dist, byte vector, byte power);
-void moveDiagonalIn(byte dist, byte vector, byte power);
-void moveDiagonalCm(byte dist, byte vector, byte power);
+//void moveDiagonalTicks(byte dist, byte vector, byte power);
+//void moveDiagonalIn(byte dist, byte vector, byte power);
+//void moveDiagonalCm(byte dist, byte vector, byte power);
 
 //Low priority: (Daniel's ideas)
-void turnSpiral(byte power, byte distance, byte deg);
+//void turnSpiral(byte power, byte distance, byte deg);
