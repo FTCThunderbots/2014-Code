669d81be6e59082f1a56926f7ad36b85ea4f66fd
Zach Ohara <zjohara@gmail.com>
2014-10-25 12:56:18 -0700

    Added WHAT?'s configuration file to /config/
    We can now test API functionality on a working platform, instead of
    waiting for the robot to be finished to debug everything.

---
+++ Config/2013_robot_config.c
@@ -0,0 +1,21 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  none)
+#pragma config(Hubs,  S2, HTServo,  HTMotor,  none,     none)
+#pragma config(Sensor, S3,     infrared,        sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S4,     touch,          sensorTouch)
+#pragma config(Motor,  mtr_S1_C1_1,     left1,          tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     right1,         tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C3_1,     left2,         tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C3_2,     right2,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     lift1,         tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_2,     lift2,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C2_1,     sweep,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C2_2,     flag,          tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S2_C1_1,    bucket,               tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_2,    pin,                  tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
+
+// Configurations for the 2013-14 robot
+// This file has been added so we can test API functionality on a working platform
