d4138fee13b993b5b0b649aa1d8986f6fd9c2edc
pranavmathur <pranavmathur99@gmail.com>
2014-10-25 12:32:00 -0700

    Daniel fixed setMovementTest.c
    Daniel did this.

--- Testing Code/setMovementTest.c
+++ Testing Code/setMovementTest.c
@@ -22,7 +22,28 @@
 
 //Config is for last year's robot
 
-#include "movement.h"
+const int JOYSTICK_MIN_VALUE = 10; //min value where the reading is accepted as an input
+const int JOYSTICK_MAX_VALUE = 127; //should only be changed if a different joystick is used
+
+// Movement
+const byte DRIVE_MIN_POWER = 0; //power cannot be in the open interval (0, min)
+const byte DRIVE_MAX_POWER = 100; //power cannot exceed max
+const byte STRAFE_MIN_POWER = 0;
+const byte STRAFE_MAX_POWER = 100;
+const byte TURN_MIN_POWER = 0;
+const byte TURN_MAX_POWER = 100;
+const float MOVE_POWER_SCALE = 1.0; //used for imposing speed limits
+
+// Motor Constants
+const byte MOTOR_MIN_POWER = 0; //not yet implemented
+const byte MOTOR_MAX_POWER = 100;
+
+void setMovement(byte forward, byte right, byte clockwise);
+void setMovement(byte forward, byte clockwise);
+static byte scaleTo(byte value, byte *range, byte *scale);
+float max(float a, float b);
+float absmax(float *a, byte len);
+float absmax(float a, float b);
 
 task main()
 {
@@ -83,4 +104,29 @@ void setMovement(byte forward, byte right, byte clockwise) {
 //for tank drive
 void setMovement(byte forward, byte clockwise) {
 	setMovement(forward, 0, clockwise);
+}
+
+static byte scaleTo(byte value, byte *range, byte *scale) {
+	if (abs(value) < *range)
+		return 0;
+	if (abs(value) > *(range+1))
+		return *(scale+1);
+	float posInR = (float)(value - *range) / *(range+2); //should be a percent
+	return (byte)(*scale) + (posInR * *(range+2));
+}
+
+float max(float a, float b) {
+	//CAUTION: may have minor imprecisions between two very close numbers
+	return a >= b ? a : b;
+}
+
+float absmax(float *a, byte len) {
+	float cummax = *a;
+	for (int i = 1; i < len; i++)
+		cummax = absmax(cummax, *(a+i));
+	return cummax;
+}
+
+float absmax(float a, float b) {
+	return max(abs(a), abs(b));
 }
