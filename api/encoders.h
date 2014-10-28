5059c97453c11ca6a4e4f719ee9d01ae6bdc4587
Daniel <danielgrimshaw42@gmail.com>
2014-10-28 10:30:02 -0700

    complies with encoders.c

--- api/encoders.h
+++ api/encoders.h
@@ -2,18 +2,6 @@
 // Header file for encoders.c
 
 // Not yet implemented:
-long inchesToTicks(float inches); //shortcut to drive ticks
-long inchesToDriveTicks(float inches);
-long inchesToStrafeTicks(float inches);
-
-long centimetersToTicks(float centimeters); //shortcut to drive ticks
-long centimetersToDriveTicks(float centimeters);
-long centimetersToStrafeTicks(float centimeters);
-
 long degreesToTicks(float degrees); //shortcut to turn ticks
 long degreesToTurnTicks(float degrees);
 long degreesToSwingTicks(float degrees);
-
-long raidansToTicks(float radians); //shortcut to turn ticks
-long radiansToTurnTicks(float radians);
-long radiansToSwingTicks(float radians);
