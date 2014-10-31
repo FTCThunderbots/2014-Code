d19141229f80120b7755cfd6762d3811b279776d
pranavmathur <pranavmathur99@gmail.com>
2014-10-31 16:39:27 -0700

    Revert "Backup lol"
    This reverts commit ed676d117aa283e841eb0307b3c12a05293f815b.

--- BACKUP/api/functions.c
+++
@@ -1,5 +0,0 @@
-// functions.c
-// Contains all functions pertaining to non-movement functions of the robot
-// Ex. Lifts, servos
-
-#include "functions.h"
--- BACKUP/api/ruler.c
+++
@@ -1,5 +0,0 @@
-// ruler.c
-// A custom encoder measurement system
-// Should be very useful
-
-#include "ruler.h"
