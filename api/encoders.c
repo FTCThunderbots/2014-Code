c5c58d402d0b492f96b2a78eb86fbedbeb7d76aa
Zach Ohara <zjohara@gmail.com>
2014-11-02 00:52:38 -0700

    Created functions to access encoder values
    Now that these functions are defined, we can finish the ruler system.

--- api/encoders.c
+++ api/encoders.c
@@ -1,6 +1,87 @@
 // encoders.c
 // Contains code for accessing encoder values at the lowest possible level
 
+// Accessing encoder values
+
+long getEncoder_left1() {
+	return nMotorEncoder[leftmotor_1];
+}
+
+long getEncoder_right1() {
+	return nMotorEncoder[rightmotor_1];
+}
+
+long getEncoder_left2() {
+	#ifndef setting_twoEncoders
+	return nMotorEncoder[leftmotor_2];
+	#else
+	return getEncoder_left1();
+	#endif
+}
+
+long getEncoder_right2() {
+	#ifndef setting_twoEncoders
+	return nMotorEncoder[rightmotor_2];
+	#else
+	return getEncoder_right1();
+	#endif
+}
+
+// Setting encoder values
+// I hope we never have to use these, but they're here just in case. (Zach)
+
+void setEncoder_left1(long val) {
+	nMotorEncoder[leftmotor_1] = val;
+}
+
+void setEncoder_right1(long val) {
+	nMotorEncoder[rightmotor_1] = val;
+}
+
+void setEncoder_left2(long val) {
+	#ifndef setting_twoEncoders
+	nMotorEncoder[leftmotor_2] = val;
+	#else
+	setEncoder_left1(val);
+	#endif
+}
+
+void setEncoder_right2(long val) {
+	#ifndef setting_twoEncoders
+	nMotorEncoder[rightmotor_2] = val;
+	#else
+	setEncoder_right1(val);
+	#endif
+}
+
+// Shortcut to getting and setting encoder values
+// Potentially useful for iterating through all the encoders?
+// Try to avoid literal string arguments, because the non-shortcut version is faster
+
+long getEncoder(String enc) {
+	if (enc == "l1")
+		return getEncoder_left1();
+	else if (enc == "r1")
+		return getEncoder_right1();
+	else if (enc == "l2")
+		return getEncoder_left2();
+	else if (enc == "r2")
+		return getEncoder_right2();
+	else
+		return -1;
+}
+		
+void setEncoder(String enc, long val) {
+	if (enc == "l1")
+		setEncoder_left1(val);
+	else if (enc == "r1")
+		setEncoder_right1(val);
+	else if (enc == "l2")
+		setEncoder_left2(val);
+	else if (enc == "r2")
+		setEncoder_right2(val);
+}
+
 // Unit conversions
 long degreesToTicks(float degrees) {
 	return degreesToTurnTicks(degrees);
--- api/encoders.h
+++ api/encoders.h
@@ -1,8 +1,25 @@
 // encoders.h
 // Header file for encoders.c
 
-//Macro Functions for length to ticks
+#define ENCODERS_INCLUDEGAURD
 
+// Accessing encoder values
+long getEncoder_left1();
+long getEncoder_left2();
+long getEncoder_right1();
+long getEncoder_right2();
+
+// Setting encoder values
+void setEncoder_left1(long val);
+void setEncoder_left2(long val);
+void setEncoder_right1(long val);
+void setEncoder_right2(long val);
+
+// Shortcut methods: useful for iterations(?)
+long getEncoder(string enc);
+void setEncoder(string enc, long val);
+
+// Unit conversions
 long degreesToTicks(float degrees);
 long degreesToTurnTicks(float degrees);
 long degreesToSwingTicks(float degrees);
