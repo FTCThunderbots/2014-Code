9cae58fa2f9902d987d2e685748d66b92c924772
Daniel <danielgrimshaw42@gmail.com>
2014-11-06 10:27:08 -0800

    Made drive_off_ramp_auto.c use our timers
    No longer has the robot c timers.

--- Autonomous/drive_off_ramp_auto.c
+++ Autonomous/drive_off_ramp_auto.c
@@ -2,13 +2,19 @@
 // The robot will start on top of the corner ramp, and drive forward
 // for 2.5 seconds, onto the floor
 
-#include "api.c"
+#define timersused 1
+#include "../api/api.c"
 
 task main()
 {
 	initializeAPI();
 	waitForStart();
+	
+	Timer_t timer;
+	pTimer_t pTimer;
+	initTimer(pTimer);
+	startTimer(pTimer);
 	drive(100);
-	wait1Msec(2500);
+	while (getDeciseconds(pTimer) < 25) {}
 	drive(0);
 }
