38da1a7a7816043c94df69eff4f276dacdf1e152
Zach Ohara <zjohara@gmail.com>
2014-11-07 15:23:35 -0800

    Revert "Revert "Revert "Depracated searchInFiles.py"""
    This reverts commit c6bf859609c0f988dea7d5ac3bca1ac7d55e1765.

--- Python/searchInFiles.py
+++ Python/searchInFiles.py
@@ -1,57 +1,42 @@
 # searchInFiles.py
 # used to search for a string of characters in a specified list of files
 
-# searchInFiles.py
-# used to search for a string of characters in a specified list of files
-print("You know, in the git shell, there is a very")
-print("useful command, called `git grep' that will")
-print("do exactly what this program does, but without")
-print("all of the hassle of python.")
-print("it is for that reason that this program has been")
-print("deprecated.")
-print("Please use git grep instead.")
-deprecated = True
+print("What phrase would you like to search for?")
+searchPhrase = input()
 
-def main():
-	print("What phrase would you like to search for?")
-	searchPhrase = input()
+#print("What folder do you want to look in? Type 'all' for all folders.")
+#searchFolder = input()
 
-	#print("What folder do you want to look in? Type 'all' for all folders.")
-	#searchFolder = input()
+print()
 
-	print()
+filesToSearch = []
+for filename in open('../List of files.txt'):
+	if len(filename) > 2:
+		filesToSearch.append(filename[:-1])
 
-	filesToSearch = []
-	for filename in open('../List of files.txt'):
-		if len(filename) > 2:
-			filesToSearch.append(filename[:-1])
-
-	'''		
-	for file in filesToSearch:
-		if (file[:len(searchFolder)-1] == searchFolder):
-			print("removed %s" % file)
-			filesToSearch.remove(file)
-			'''
+'''		
+for file in filesToSearch:
+	if (file[:len(searchFolder)-1] == searchFolder):
+		print("removed %s" % file)
+		filesToSearch.remove(file)
+		'''
 		
-	#print(filesToSearch)
-	found = []
+#print(filesToSearch)
+found = []
 
-	for file in filesToSearch:
-		openedFile = open('../' + file)
-		lineNum = 0
-		for line in openedFile:
-			lineNum += 1
-			if searchPhrase in line:
-				found.append([file, lineNum])
+for file in filesToSearch:
+	openedFile = open('../' + file)
+	lineNum = 0
+	for line in openedFile:
+		lineNum += 1
+		if searchPhrase in line:
+			found.append([file, lineNum])
 			
-	#print(found)
+#print(found)
 
-	if len(found) > 0:
-		print("\"%s\" appears at..." % searchPhrase)
-		for place in found:
-			print("Line %d in %s" % (place[1], place[0]))
-	else:
-		print("Sorry, but %s was not found in any known files" % searchPhrase)
-		
-if deprecated != False:
-	main()
+if len(found) > 0:
+	print("\"%s\" appears at..." % searchPhrase)
+	for place in found:
+		print("Line %d in %s" % (place[1], place[0]))
+else:
+	print("Sorry, but %s was not found in any known files" % searchPhrase)
