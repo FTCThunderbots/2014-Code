4b156ef217215ee47ac1f88fffa8f7842b7786eb
Zachary Waldron <wewingames222@gmail.com>
2014-11-08 11:10:33 -0800

    Added Coordinate Driving Test
    This will test the coordinate system.

---
+++ Testing/CoordinateDrivingTest.c
@@ -0,0 +1,29 @@
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S3,     infrared,       sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S4,     touch,          sensorTouch)
+#pragma config(Motor,  mtr_S1_C1_1,     leftmotor_1,          tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     rightmotor_1,         tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     Lift1,         tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_2,     Lift2,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     Left2,         tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C3_2,     Right2,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C2_1,     Sweep,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C2_2,     Flag,          tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S2_C1_1,    bucket,               tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_2,    pin,                  tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
+
+#include "coord_test.c"
+
+void testCoords() {
+	gotoCoordinates(1, 0, 180);
+	gotoCoordinates(0, 0, 0);
+	gotoCoordinates(0, 1, 180);
+
+	gotoCoordinates(0, 0, 0);
+	getCoordinateValue(1);
+}
--- Testing/coord_test.c
+++ Testing/coord_test.c
@@ -1,6 +1,41 @@
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S3,     infrared,       sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S4,     touch,          sensorTouch)
+#pragma config(Motor,  mtr_S1_C1_1,     leftmotor_1,          tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     rightmotor_1,         tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     Lift1,         tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_2,     Lift2,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     Left2,         tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C3_2,     Right2,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C2_1,     Sweep,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C2_2,     Flag,          tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S2_C1_1,    bucket,               tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_2,    pin,                  tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
+
+
+// Pragma configurations will need to go here before anything can be done, for testing
+
 // #include statements
 #include "JoystickDriver.c"
 #include "unit_converter.c"
+//#include "../api/api.c"
+//to drive say
+//drive(100)
+
+//to turn, say
+//rotate(100)
+/*
+int declareAnyVariableHere = 0;
+#define DEBUG_STREAM_ON
+#define debugStreamLine1 declareAnyVariableHere
+#define debugStreamLine2 anyOtherVariable
+//up to 8
+*/
 
 #define XCOORD 0
 #define ZCOORD 1
@@ -11,29 +46,34 @@ float x = 0.0, z = 0.0;
 float orientation = 0.0;
 float changeCoord = 0.0;
 
+#define DEBUG_STREAM_ON
+#define debugStreamLine1	x
+#define debugStreamLine2	z
+
 static float updateX(float degrees, float distanceTraveled) {
-	return (float)(x + sin(degrees) * distanceTraveled);
+	return (float)(x + sin(degrees) * distanceTraveled); //remove cast
 }
 
 static float updateZ(float degrees, float distanceTraveled) {
 	return (float)(z + cos(degrees) * distanceTraveled);
 }
 
-static float getOrientation(float degrees) {
+float getOrientation(float degrees) {
 	// will return off a base of 4
 	// orientation will always be equal to theta, and will return in DEGREES
 	// It is up to the programmer to deal with the degrees
 	return (float)(degrees * (180 / PI));
 }
 
-static void updateCoords(float degrees, length_t CIRCUM_TYPE) {
+void updateCoords(float degrees, length_t CIRCUM_TYPE) {
 		/*
 			1. Divide ticks by 1440 then multiply by circumference (Gives you how far it moved in the units
 			you have used for circumference.
 			2. Convert the units to feet.
 		*/
-		byte ticks = nMotorEncoder[NULL]; /* Will remain NULL until motor is known.*/
-		changeCoord = (ticks / 1440) * convertUnits(CIRCUM_TYPE, FEET, degrees);
+		byte ticks = nMotorEncoder[leftmotor_1]; /* Will remain NULL until motor is known.*/
+		ticks += nMotorEncoder[rightmotor_1];
+		ticks /= 2;
 
 		/*
 		*First I take the degrees and convert to radians.
@@ -52,7 +92,7 @@ static void updateCoords(float degrees, length_t CIRCUM_TYPE) {
 		changeCoord = 0.0;
 }
 
-static float getCoordinateValue(int value) {
+float getCoordinateValue(int value) {
 	if (value == 0) {
 			return x;
 	} else if (value == 1) {
@@ -62,7 +102,7 @@ static float getCoordinateValue(int value) {
 	}
 }
 
-static void gotoCoordinates(float newX, float newZ, float newOrientation) {
+void gotoCoordinates(float newX, float newZ, float newOrientation) {
 	/*
 		For now I will go in the following order:
 			1.) I will go to the correct X value
@@ -86,10 +126,12 @@ static void gotoCoordinates(float newX, float newZ, float newOrientation) {
 	}
 
 	while (x < newX + 1 || x > newX - 1) {
-		byte ticks = nMotorEncoder[NULL]; /* Will remain NULL until motor is known.*/
+		byte ticks = nMotorEncoder[leftmotor_1]; /* Will remain NULL until motor is known.*/
+		ticks += nMotorEncoder[rightmotor_1];
+		ticks /= 2;
 		changeCoord = (ticks / 1440) * convertUnits(INCHES, FEET, PI / 4);
 		updateX(PI / 4, changeCoord);
-		turn45(false);
+		drive(50);
 	}
 
 	if (newZ < z) {
@@ -107,10 +149,12 @@ static void gotoCoordinates(float newX, float newZ, float newOrientation) {
 	}
 
 	while (z < newZ + 1 || z > newZ - 1) {
-		byte ticks = nMotorEncoder[NULL]; /* Will remain NULL until motor is known.*/
+		byte ticks = nMotorEncoder[leftmotor_1]; /* Will remain NULL until motor is known.*/
+		ticks += nMotorEncoder[rightmotor_1];
+		ticks /= 2;
 		changeCoord = (ticks / 1440) * convertUnits(INCHES, FEET, PI / 4);
 		updateZ(PI / 4, changeCoord);
-		turn45(false);
+		drive(50);
 	}
 
 
