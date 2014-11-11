49c22818bcd4a7f7c108787b1aef54e4d79ed3d0
Zach Ohara <zjohara@gmail.com>
2014-11-11 14:36:21 -0800

    Updated config on main teleop
    Also a helpful diagram has been included

--- Tele-Op/main_teleop.c
+++ Tele-Op/main_teleop.c
@@ -1,9 +1,33 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     leftmotor_1,   tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     rightmotor_1,  tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_1,     leftmotor_2,   tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_1,     leftmotor_1,   tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     leftmotor_2,   tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     rightmotor_1,  tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C2_2,     rightmotor_2,  tmotorTetrix, openLoop)
+
+#define setting_twoEncoders
+
+/* How to set up the robot based on this configurations:
+
+
+                        FRONT
+
+(mtr_S1_C1_1) |  |-----------------|  | (mtr_S1_C2_1)
+leftmotor_1   |--|                 |--|  rightmotor_1
+encoder here  |  |                 |  |  encoder here
+                 |                 |
+                 |                 |
+                 |***           ***|
+  mtr_s1_c1 -->  |***           ***|  <-- mtr_s1_c2
+                 |                 |
+                 |                 |
+(mtr_S1_C1_2) |  |                 |  | (mtr_S1_C2_2)
+leftmotor_2   |--|                 |--|  rightmotor_2
+              |  |-----------------|  |
+
+                        BACK
+
+*/
+
 
 // main teleop.c
 // Main tele-op program
@@ -14,7 +38,7 @@ task main() {
 	initializeAPI();
 	while (true) {
 		getJoystickSettings(joystick);
-		setMovementFromJoystick(joystick.joy1_y1, joystick.joy1_x1, joystick.joy1_x2);
+		setMovementFromJoystick(joystick.joy1_y1, joystick.joy1_x2);
 		wait1Msec(10);
 	}
 }
