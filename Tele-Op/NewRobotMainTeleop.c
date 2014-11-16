722b6565f924cf3e3de7f339d24e43c9ae807699
Pranav Mathur <pranavmathur99@gmail.com>
2014-11-16 11:32:21 -0800

    Implemented Old Move, Now Works
    Works

--- Tele-Op/NewRobotMainTeleop.c
+++ Tele-Op/NewRobotMainTeleop.c
@@ -16,6 +16,8 @@ bool userIsAGoat() {
 	return false;
 }
 
+void setOldMovementPower(int power, int turn);
+
 bool userIsNotAGoat() {
 	return !userIsAGoat();
 }
@@ -29,23 +31,36 @@ task main()
 		int a = 0;
 		int b = 0;
 		getJoystickSettings(joystick);
-		backupSetMovementJoystick(-joystick.joy1_y1, -joystick.joy1_x2);
-		if (!(joy1Btn(6) || joy1Btn(8) || joy1Btn(5) || joy1Btn(7)))
-			motor[sweep] = 0;
-		else if (joy1Btn(6))
+		setOldMovementPower(joystick.joy1_y1, joystick.joy1_x2);
+		if (joy1Btn(2)) {
 			motor[sweep] = 100;
-		else if (joy1Btn(8))
-			motor[sweep] = 50;
-		else if (joy1Btn(5))
-			motor[sweep] = -100;
-		else
-			motor[sweep] = -50;
+			motor[conveyor] = 100;
+		}
+		else {
+			if (!(joy2Btn(6) || joy2Btn(8) || joy2Btn(5) || joy2Btn(7)))
+				motor[sweep] = 0;
+			else if (joy2Btn(6))
+				motor[sweep] = 100;
+			else if (joy2Btn(8))
+				motor[sweep] = 50;
+			else if (joy2Btn(5))
+				motor[sweep] = -100;
+			else
+				motor[sweep] = -50;
+
 
 		a = joystick.joy2_y1;
+		if (abs(a)<10) a = 0;
 		b = 100;
 		scaleInputs(&a, &b);
 		motor[conveyor] = a/2;
-		nxtDisplayCenteredBigTextLine(5, "%d", motor[conveyor]);
+	}
+		nxtDisplayCenteredTextLine(5, "conveyor: %d", motor[conveyor]);
+		nxtDisplayCenteredTextLine(0, "sweep: %d", motor[sweep]);
+		nxtDisplayCenteredTextLine(1, "left1: %d", motor[leftmotor_1]);
+		nxtDisplayCenteredTextLine(2, "left2: %d", motor[leftmotor_2]);
+		nxtDisplayCenteredTextLine(3, "r1: %d", motor[rightmotor_1]);
+		nxtDisplayCenteredTextLine(4, "r2: %d", motor[rightmotor_2]);
 	}
 }
 
@@ -55,4 +70,37 @@ void backupSetMovementJoystick(byte foward, byte turn){
 	foward = (byte)((float)foward * (100.0/128.0));
 	turn = (byte)((float)turn * (100.0/128.0));
 	setMovement(foward,turn);
+}
+
+void setOldMovementPower(int power, int turn){
+	if (abs(power) < 10) power = 0;
+	if (abs(turn) < 10) turn = 0;
+	//turn = turn * 1.5;
+	int leftBrake = 1;
+	int rightBrake = 1;
+	if (joy1Btn(7)){
+		leftBrake = 0;
+		//turn = 0;
+	}
+	if (joy1Btn(8)){
+		rightBrake = 0;
+		//turn = 0;
+	}
+	float leftFinal = (float) leftBrake * (power + turn);
+	float rightFinal = (float) rightBrake * (-power + turn);
+
+	if (abs(leftFinal) > 100)
+	{
+		rightFinal *= 100 / abs(leftFinal);
+		leftFinal *= 100 / abs(leftFinal);
+	}
+	if (abs(rightFinal) > 100)
+	{
+		leftFinal *= 100 / abs(rightFinal);
+		rightFinal *= 100 / abs(rightFinal);
+	}
+	motor[leftmotor_1] = leftFinal;
+	motor[rightmotor_1] = rightFinal;
+	motor[leftmotor_2] = leftFinal;
+	motor[rightmotor_2] = rightFinal;
 }
