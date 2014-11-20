e2fb2f056a4d697675ce14d9d420b5f0f41ad2e2
Zach Ohara <zjohara@gmail.com>
2014-11-20 00:05:02 -0800

    Removed macros, deprecated a function macro
    POLARITY() does the same thing as the built-in sgn()

--- api/math.h
+++ api/math.h
@@ -6,14 +6,13 @@
 #define IN_PER_FT 12
 #define CM_PER_IN 2.54
 #define CM_PER_M 100
-#define NUMBER_OF_WHEELS 3
 
 //Function Macros
 
 #define AVG(a, b) (a+b)/2
 #define MAX(a, b) (a >= b ? a : b)
 #define ABSMAX(a, b) (MAX(abs(a), abs(b)))
-#define POLARITY(n) n!=0?abs(n)/n:1
+#define POLARITY(n) n!=0?abs(n)/n:1 //deprecated, use sgn()
 
 // Maximum value functions
 
