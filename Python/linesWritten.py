7640b640da0ef51d354d69bca6ab93b5350926b0
Daniel <danielgrimshaw42@gmail.com>
2014-11-21 10:32:01 -0800

    Deprecated linesWritten.py and fixed file list
    wc -l $(cat "./List of Files.txt")
    When run from the home directory (of this repo) serves the same purpose.
    Also discovered that the line counts are inaccurate. We have about 5.65
    KiB of non-code lines in this repo.
    
    `wc --help' will list all other counting options.

--- Python/linesWritten.py
+++ Python/linesWritten.py
@@ -3,6 +3,16 @@
 
 # Please keep this list alphebetized
 
+print("This program has been deprecated!")
+print("The command `wc -l $(git ls-files)'")
+print("Performs the same function. If you")
+print("only want to read the lines of executable")
+print("code use the command: ")
+print("`wc -l $(cat \".\List of Files.txt\"'")
+print("\nUnlike searchInFiles.py this program will")
+print("not be deleted as the commands are not simple.")
+print("\nCAUTION!!! The count is not accurate!")
+
 def lengthOf(filename):
 	try:
 		return sum(1 for line in open('../' + filename))
@@ -19,3 +29,6 @@ for filename in open('../List of files.txt'):
 		lines += lengthOf(file)
 	
 print("All the specified files contain a total of %d lines of code" % lines)
+
+#wc can also count words, bytes, length of longes line, and characters.
+#The last time I (Daniel) checked we had about 100 KiB of code (99.88281250 KiB) and 10708 words.
