43e1fe8024ef66999abb7d79634f326f01bd0636
Zach Ohara <zjohara@gmail.com>
2014-11-21 18:28:13 -0800

    Fixed main teleop config

--- Tele-Op/main_teleop.c
+++ Tele-Op/main_teleop.c
@@ -1,10 +1,12 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTServo)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Motor,  mtr_S1_C1_1,     leftmotor_1,   tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     leftmotor_2,   tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C2_1,     rightmotor_1,  tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C2_2,     rightmotor_2,  tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C3_1,     conveyor,      tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C3_2,     sweep,         tmotorTetrix, openLoop, reversed)
+#pragma config(Servo,  srvo_S1_C4_1,    backboard,            tServoStandard)
 
 #define setting_twoEncoders
 
