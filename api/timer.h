f93d27418b0d1b4b5b0371cfba2c8ffb06f4b125
Daniel <danielgrimshaw42@gmail.com>
2014-12-23 12:28:00 -0800

    Revert "Deleted timer.h"
    This reverts commit e601466cb11d6178e6af6742d82de44633f0e602.

---
+++ api/timer.h
@@ -0,0 +1,11 @@
+// timer.h
+// all of the delaying functions
+
+//macros
+#define TIMER_INCLUDEGUARD
+
+//function macros
+#define waitMilliseconds(Msecs) wait1Msec(Msecs)
+#define waitCentiseconds(Csecs) wait1Msec(Csecs*10)
+#define waitDeciseconds(Dsecs) wait1Msec(Dsecs*100)
+#define waitSeconds(secs) wait10Msec(secs*100)
