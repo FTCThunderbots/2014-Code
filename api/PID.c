1e1dd9032ee71b5235750f3c382c49ae97d65cab
Daniel <danielgrimshaw42@gmail.com>
2014-12-27 11:40:11 -0800

    Updated all functions in PID
    All of them will not exceed max speed, also updated swing to work, in
    theory, for negative values

--- api/PID.c
+++ api/PID.c
@@ -1,11 +1,11 @@
 // PID.c
 //This is where we code the PID.
 
-void correctLinear() {
+void correctLinear(int speed) {
 
 }
 
-void correctRotate() {
+void correctRotate(int posSpeed) {
 
 }
 
@@ -13,24 +13,24 @@ void correctRotate() {
 void correctSwing(int speed) {
 	if (motor[leftmotor_1] != 0) {
 		if (leftEnc1 < leftEnc2) {
-			if (motor[leftmotor_1] < speed - 4)
+			if (abs(motor[leftmotor_1]) < abs(speed) - 4)
 				motor[leftmotor_1] += CORRECTION_CONSTANT;
 			else
 				motor[leftmotor_2] -= CORRECTION_CONSTANT;
 		} else if (leftEnc1 > leftEnc2) {
-			if (motor[leftmotor_2] < speed - 4)
+			if (abs(motor[leftmotor_2]) < abs(speed) - 4)
 				motor[leftmotor_2] += CORRECTION_CONSTANT;
 			else
 				motor[leftmotor_1] -= CORRECTION_CONSTANT;
 		}
 	} else {
 		if (rightEnc1 < rightEnc2) {
-			if (motor[rightmotor_1] < speed - 4)
+			if (abs(motor[rightmotor_1]) < abs(speed) - 4)
 				motor[rightmotor_1] += CORRECTION_CONSTANT;
 			else
 				motor[rightmotor_2] -= CORRECTION_CONSTANT;
 		} else if (rightEnc1 > rightEnc2) {
-			if (motor[rightmotor_2] < speed - 4)
+			if (abs(motor[rightmotor_2]) < abs(speed) - 4)
 				motor[rightmotor_2] += CORRECTION_CONSTANT;
 			else
 				motor[rightmotor_1] -= CORRECTION_CONSTANT;
--- api/PID.h
+++ api/PID.h
@@ -4,8 +4,8 @@
 
 #define PID_INCLUDEGUARD
 
-void correctLinear(); //the correction algorithm for linear movement (driving)
-void correctRotate(); //the correction algorithm for rotation
+void correctLinear(int speed); //the correction algorithm for linear movement (driving)
+void correctRotate(int posSpeed); //the correction algorithm for rotation
 
 #ifndef setting_twoMotors
 void correctSwing(int speed); //corrects for swing, will only be necessary if there is a four wheel drive
