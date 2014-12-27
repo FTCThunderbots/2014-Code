f9148d8cf10583e99c1c4f0a5f43cd485346d8d4
Zachary Waldron <wewingames222@gmail.com>
2014-12-27 11:18:32 -0800

    Finished coord system

---
+++ Testing/coord.c
@@ -0,0 +1,25 @@
+#include "api.c"
+#include "coord.h"
+
+#define COORD_INCLUDEGAURD	true
+
+float x, z, orientation;
+
+void updateDirection(float degrees) {
+	orientation = (180 / PI) * orientation + degrees;
+	orientation = degreesToRadians(orientation);
+}
+
+void updateCoords(float distance) {
+	x = distance * sin(orientation) + x;
+	z = distance * cos(orientation) + z;
+}
+
+float readValues(char value) {
+	if (value == 'x')
+		return x;
+	else if (value == 'z')
+		return z;
+	else
+		return orientation;
+}
---
+++ Testing/coord.h
@@ -0,0 +1,3 @@
+void updateDirection(float degrees);
+void updateCoords(float distance);
+float readValues(char value);		/*'x' for x 'z' for z 'o' for orientation*/
