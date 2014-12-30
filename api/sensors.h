74bf7c23c14c35c8069f8cc1293e64ea1d08ae05
danielgrimshaw <danielgrimshaw42@gmail.com>
2014-12-30 13:10:18 -0800

    Added Sensor Drivers
    Also updated all of the sensor files. I'm going to pretend that I wrote
    all of this code.

--- api/sensors.h
+++ api/sensors.h
@@ -2,3 +2,7 @@
 // Header file for sensors.c
 
 #define SENSORS_INCLUDEGUARD
+
+#include "Xander_Drivers\drivers\hitechnic-sensormux.h" //We will be using these drivers for all of our sensors
+																										 		//This is the sensor multiplexor
+#include "Xander_Drivers\drivers\hitechnic-irseeker-v2.h" // This is the IR seeker
