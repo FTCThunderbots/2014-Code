0cc26babab7ce7b6f8f51bd1c8c5d7f27c4a7e4a
Zachary Waldron <wewingames222@gmail.com>
2014-12-30 18:32:50 -0800

    Added Debug Info To Autonomous

--- Autonomous/autoV1.c
+++ Autonomous/autoV1.c
@@ -49,4 +49,10 @@ task main()
     releaseGoal_time();
 
     setMovement(0, 0);
+
+    while (true) {
+		nxtDisplayCenteredTextLine(0, "X is: %0.01f", readValues('x'));
+		nxtDisplayCenteredTextLine(1, "Z is: %0.01f", readValues('z'));
+		nxtDisplayCenteredTextLine(2, "O is: %0.01f", readValues('o'));
+	}
 }
