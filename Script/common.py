8435103689b8c96377ee8891600829844a2687a7
Zach Ohara <zjohara@gmail.com>
2015-01-04 00:43:07 -0800

    Auto-update spacing in script/common.py
    Not really sure, but I think it reset all my indentation and replaced it
    with... different indentation?

--- Script/common.py
+++ Script/common.py
@@ -9,24 +9,27 @@ ACCEPTED_EXTENSIONS = ['.c', '.h', '.py']
 
 #A generator that returns all the files in the repo matching a given set of conditions
 def getFiles(condition):
-	for dirname, dirnames, filenames in os.walk('..'):
-		if not dirnameIsBanned(dirname):
-			for file in filenames:
-				if not filenameIsBanned(file, dirname):
-					if condition(file):
-						yield os.path.join(dirname, file)
+   for dirname, dirnames, filenames in os.walk('..'):
+      if not dirnameIsBanned(dirname):
+         for file in filenames:
+            if not filenameIsBanned(file, dirname):
+               if condition(file):
+                  yield os.path.join(dirname, file)
 						
 					#if file.endswith(".c"):
 						#yield os.path.join(dirname, file)
 						
 def dirnameIsBanned(dirname):
-	dirname = dirname.lower()
-	return (True in [("\\"+exFolder+"\\").lower() in dirname or dirname.endswith(("\\"+exFolder).lower()) for exFolder in EXCLUDED_FOLDERS])
+   dirname = dirname.lower()
+   return (True in [("\\"+exFolder+"\\").lower() in dirname or dirname.endswith(("\\"+exFolder).lower()) for exFolder in EXCLUDED_FOLDERS])
 	
 def filenameIsBanned(filename, dirname):
-	filename = filename.lower()
-	dirname = dirname.lower()
-	return (True in [os.path.join(dirname, filename) == os.path.join("..",exFile) for exFile in EXCLUDED_FILES])
+   filename = filename.lower()
+   dirname = dirname.lower()
+   return (True in [os.path.join(dirname, filename) == os.path.join("..",exFile) for exFile in EXCLUDED_FILES])
 	
 def isValidCode(file):
-	return True in [file.endswith(ex) for ex in ACCEPTED_EXTENSIONS]
+   return True in [file.endswith(ex) for ex in ACCEPTED_EXTENSIONS]
+	
+def noCondition(file):
+   return True #useful for accepting all files
