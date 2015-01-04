59eccd8b05f1b0d3529dd51bf25494bbd501de88
Zach Ohara <zjohara@gmail.com>
2015-01-04 02:16:11 -0800

    Completed autofixWhitespace.py
    Running this script will fix all the whitespace in the entire repo, with
    the exception of .git folders, and Xander's drivers

--- Script/autofixWhitespace.py
+++ Script/autofixWhitespace.py
@@ -3,17 +3,48 @@
 
 import common
 
+SINGLELOOPS = ("if", "while", "for", "else", "else if")
+
 def main():
 	modified = 0
 	for filename in common.getFiles(common.fileIsC):
 		indent = ''
-		for line in open(filename, 'r+'):
+		singleindent = ''
+		corrected = []
+		#try:
+		file = open(filename, 'r')
+		for line in file:
+			if '}' in line and '{' not in line:
+				indent = indent[:-1]
 			i = 0
 			while line[i] in [' ', '\t']:
 				i += 1
-			line = indent + line[i:]
+			corrected.append(indent + singleindent + line[i:])
 			if '{' in line:
 				indent += '\t'
-			if '}' in line:
-				indent = indent[:-2]
-		
+				if '}' in line:
+					indent = indent[:-1]
+			if isSingleLoop(line) and '{' not in line:
+				singleindent += '\t'
+			else:
+				singleindent = ''
+		file.close()
+		file = open(filename, 'w')
+		file.truncate()
+		for line in corrected:
+			file.write(line)
+		file.close()
+		modified += 1
+		#except:
+			#print("Error:", filename, "could not be edited")
+	print("%d files were corrected" % modified)
+	
+def isSingleLoop(line) :
+	for loop in SINGLELOOPS:
+		if ('\t' + loop + ' ') in line:
+			return True
+		if ('\t' + loop + '\n') in line:
+			return True
+
+
+main()
