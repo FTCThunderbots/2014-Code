7834c621e570c5ad38f1920c0706586d6e98aa94
danielgrimshaw <danielgrimshaw42@gmail.com>
2015-01-05 18:40:22 -0800

    Now you can see what I mean
    No, autofix does not read the comments and follow what they say to do, although that would be pretty cool.

--- Script/experimental/test.c
+++ Script/experimental/test.c
@@ -18,6 +18,6 @@ while (/*for example true*/false)
 * this shouldn't be indented
 for */ this.isGettingWeird /*Yep, it is.*/ /*All of this code is unseen by the comment checker*/
 /*
-Therefore,*/I put this here /*for demonstration purposes,
-but I don't think it really needs to be a multiline comment*/
+Therefore,*/I put this here 
+but I don't think it really needs to be a multiline comment*/ //apparently neither did autofix. (I manually added this comment, it was not autofix)
 //testing purposes only
