006ac88348ee5cb61fb1babfd283a8d6f42ab31c
Pranav Mathur <pranavmathur99@gmail.com>
2015-04-15 15:35:39 -0700

    Cleaned up minibot files

--- Testing/auto_instruction_reader.h
+++
@@ -1,4 +0,0 @@
-const int stdDel = 500; // standard delay
-const int stdPow = 50; // standard power
-
-void a(byte instruction) {}
--- Testing/compass_sensor.h
+++
@@ -1,6 +0,0 @@
-int getCompassReading();
-int calculateAverageReading(int samples);
-void alignToCompassZero();
-void alignToInitial();
-void rotateDegreesImp(int degrees, byte power);
-// compass_sensor.h Header File (Does indeed use the coordinate system!)
